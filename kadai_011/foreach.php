4175ba5d98a9057dad3bbfe78eff82a739fd10ac
tonkori5 <49616356+tonkori5@users.noreply.github.com>
2025-01-29 15:12:03 +0900

    kadai_011

---
+++ kadai_011/foreach.php
@@ -0,0 +1,13 @@
+<p>
+  <?php
+  $vegetable = [
+    '名前' => '玉ねぎ',
+    '値段' => 200,
+    '産地' => '北海道'
+  ];
+
+  foreach ($vegetable as $key => $value) {
+    echo "{$key} : {$value}<br>";
+  }
+  ?>
+</p>
